a1dc741b8679f071569b43af8a0e9c27fbecc667
Anne Lee <hermy0211@naver.com>
2019-03-09 14:59:27 +0900

    Updates

---
+++ precache-manifest.8b6ebdbdd72c4c1ffdbfb4565804f6f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.784d01ab.js"
+  },
+  {
+    "revision": "74fec555af04c6ff6f64",
+    "url": "/portfolio/static/js/main.067e01e5.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/portfolio/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "74fec555af04c6ff6f64",
+    "url": "/portfolio/static/css/main.45861c73.chunk.css"
+  },
+  {
+    "revision": "4f9a5e683aa6c9ed85810ed51d7f7a8b",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cd7b99790382a269d333c4ae24398f2.js"
+  "/portfolio/precache-manifest.8b6ebdbdd72c4c1ffdbfb4565804f6f6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
